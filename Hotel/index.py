008c0613fab15187413e73e7ef6d3950cdff2341
DoanGiaHuyIT02 <87509680+DoanGiaHuyIT02@users.noreply.github.com>
2022-11-23 16:33:37 +0700

    main

--- Hotel/index.py
+++ Hotel/index.py
@@ -32,6 +32,10 @@ def rooms():
 def register():
     return render_template('register.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
